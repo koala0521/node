d736ef4b7f2705375fbabe4f9fa896011355bc6e
koala0521 <328762691@qq.com>
2020-08-20 16:30:56 +0800

    企业级架构--路由模块化

--- express/10/router/admin.js
+++ express/10/router/admin.js
@@ -2,7 +2,7 @@
  * @Author: XueBaBa
  * @Description: 文件描述~
  * @Date: 2020-08-20 15:59:45
- * @LastEditTime: 2020-08-20 16:16:37
+ * @LastEditTime: 2020-08-20 16:26:48
  * @LastEditors: Do not edit
  * @FilePath: /express/10/router/admin.js
  */
@@ -10,11 +10,13 @@ const express = require('express');
 let router = express.Router();
 const user = require('./admin/user');
 const nav = require('./admin/nav');
+const login = require('./admin/login');
 
 // 挂载路由
 
 router.use("/user",user);
 router.use("/nav",nav);
+router.use("/login",login);
 
 router.get("/",(req,res)=>{
     res.send("后台路由~~~")
